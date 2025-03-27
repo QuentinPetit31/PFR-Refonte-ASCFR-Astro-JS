242e270abbaf518c4c2b49fcf44b83bc7f1bf489
QuentinPetit31 <140169105+QuentinPetit31@users.noreply.github.com>
2025-03-27 12:10:42 +0100

    ajout cypress

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./public/**/*.html",
+    "./src/styles/style.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
